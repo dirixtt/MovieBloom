7b16a3526b3359363d747f152230fae4dbd68649
Shoxjahon1302 <shoxjahonniyatov@gmail.com>
2023-09-21 20:24:07 +0500

    user register login page end

---
+++ Server/models/user.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const user = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", user);
+export default User;
---
+++ Server/routes/userRoute.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const user_api = express.Router();
+const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middleware/authMiddleware.js");
+// user registration
+user_api.post("/register", async (req, res) => {
+  try {
+    //check if user already exists
+    const userExists = await User.findOne({ email: req.body.email });
+    if (userExists) {
+      return res
+        .status(200)
+        .send({ message: "Foydalanuvchi allaqachon mavjud", success: false });
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+
+    //create new user
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.send({
+      message: "Foydalanuvchi muvaffaqiyatli yaratildi",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+      data: error,
+      success: false,
+    });
+  }
+});
+// user login
+user_api.post("/login", async (req, res) => {
+  try {
+    //check if user exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res
+        .status(200)
+        .send({ message: "Foydalanuvchi mavjud emas", success: false });
+    }
+
+    // check password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res
+        .status(200)
+        .send({ message: "Yaroqsiz parol", success: false });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.send({
+      message: "Foydalanuvchi muvaffaqiyatli tizimga kirdi",
+      success: true,
+      data: token,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+      data: error,
+      success: false,
+    });
+  }
+});
+//get user info
+user_api.post("/get-user-info", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userId);
+    res.send({
+      message: "Foydalanuvchi maʼlumotlari muvaffaqiyatli olindi",
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+      data: error,
+      success: false,
+    });
+  }
+});
+export default user_api;
